63d125a217d6d2642c74cb5663b9eaef0a583998
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-04-02 04:59:50 +0500

    Udpate

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 #!/usr/bin/env node
+import inquirer from "inquirer";
+const str = await inquirer.prompt([
+    {
+        type: "input",
+        message: "Enter Your Paragrahp Or Sentence for Word Counting > ",
+        name: "inputString"
+    }
+])
+let WordCount = str.inputString.trim().split(/\s+/).lenght
+console.log('WordCount', WordCount)
